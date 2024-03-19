e810f7255c383ae734c09d9cc2c26d15d1c45372
Syntax Garden <73671286+syntaxGarden@users.noreply.github.com>
2024-03-19 02:05:33 +0000

    Delete project_folder directory

--- project_folder/scripts/Global.cs
+++
@@ -1,8 +0,0 @@
-using Godot;
-using System;
-
-public partial class Global : Node
-{
-    public string ScriptID { get { return GetScriptID(); } }
-    internal string GetScriptID() { return "This is a global script that will be autoloaded."; }
-}
